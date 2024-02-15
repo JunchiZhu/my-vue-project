a7b4f89d03263ba5dce73641f89215779dbb999c
zhujunch <zhujunch@msu.edu>
2024-02-14 20:43:08 -0500

    Update Google Analytics integration again

--- src/components/UserActivityTracker.vue
+++ src/components/UserActivityTracker.vue
@@ -3,16 +3,32 @@
     <input v-model="inputValue" placeholder="Enter text here" @input="trackInput">
     <button @click="buttonClicked">Click Me</button>
   </div>
+
+  <div>
+  <button @click="incrementCount">Click Me</button>
+  <p>You have clicked the button {{ count }} times.</p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      count: 0,
       inputValue: ''
     }
   },
   methods: {
+    incrementCount() {
+      this.count++;
+      // Send an event to Google Analytics
+      this.$gtag.event('button_click', {
+        event_category: 'Button Interactions',
+        event_label: 'Click Me Button',
+        value: this.count,
+      });
+      console.log(`Button clicked ${this.count} times`);
+    },
     buttonClicked() {
       console.log("Button clicked with input:", this.inputValue);
       // Example tracking code (replace with actual Google Analytics tracking code)

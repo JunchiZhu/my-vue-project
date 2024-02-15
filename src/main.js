18cb6e562290bc1f614e86842f3e5a932cb43ea9
zhujunch <zhujunch@msu.edu>
2024-02-14 20:33:58 -0500

    Update Google Analytics integration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueGtag, {
+    config: { id: "G-7MN862MB30" }
+});
+
+app.mount('#app');

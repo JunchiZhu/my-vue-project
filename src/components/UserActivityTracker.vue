e5536f3765d36d053739c5706052e17fe33b6b37
zhujunch <zhujunch@msu.edu>
2024-02-14 19:48:37 -0500

    Initial commit

---
+++ src/components/UserActivityTracker.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input v-model="inputValue" placeholder="Enter text here" @input="trackInput">
+    <button @click="buttonClicked">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: ''
+    }
+  },
+  methods: {
+    buttonClicked() {
+      console.log("Button clicked with input:", this.inputValue);
+      // Example tracking code (replace with actual Google Analytics tracking code)
+      // this.$gtag.event('button_click', {
+      //   event_category: 'User Actions',
+      //   event_label: 'Click Me Button',
+      //   value: this.inputValue
+      // });
+    },
+    trackInput() {
+      // Logic to track input (for demonstration purposes, actual tracking would require integration with analytics)
+      console.log("Tracking input:", this.inputValue);
+    }
+  }
+}
+</script>
